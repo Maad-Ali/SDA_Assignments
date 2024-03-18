5d8082ef5a20d0cdc99bebe49be111e91b5f3f71
Maad-Ali <maad.alshaik@gmail.com>
2024-03-18 14:53:23 +0300

    Added Task#4

--- src/test/java/TestNG/FourthTask.java
+++ src/test/java/TestNG/FourthTask.java
@@ -2,39 +2,62 @@ package TestNG;
 
 import TestPackage.TestBase;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-
-public class FourthTask extends TestBase {
+public class FourthTask  {
+    WebDriver driver;
     //Open the site: http://opencart.abstracta.us/index.php?route=account/login
     //Login with that credentials
     //Email: clarusway@gmail.com
     //Password: 123456789
     //Using the Search function do it with Data Provider for Mac, iPad and Samsung.
-    WebDriver driver;
+
+    @BeforeClass
+    public void BeforeClass(){
+        driver=new ChromeDriver();
+        driver.get("http://opencart.abstracta.us/index.php?route=account/login");
+    }
     @DataProvider(name = "searchData")
-    public Object[][] searchData() {
-        return new Object[][]{
+    public Object[][] dataProvider() {
+        return new Object[][] {
                 {"Mac"},
                 {"iPad"},
                 {"Samsung"}
         };
     }
-    @Test(dataProvider = "searchData")
-    public void DataProviderTest(String input){
+    @Test(priority = 1)
+    public void login() {
+        By EmailField= By.id("input-email");
+        By PassWord= By.id("input-password");
+        By LoginButton=By.xpath("//input[@type='submit']");
 
-    By EmailField= By.id("input-email");
-    By PassWord= By.id("input-password");
-    By LoginButton=By.xpath("//input[@class='btn btn-primary']");
+        driver.findElement(EmailField).sendKeys("clarusway@gmail.com");
+        driver.findElement(PassWord).sendKeys("123456789");
+        driver.findElement(LoginButton).click();
 
-    driver.findElement(EmailField).sendKeys("clarusway@gmail.com");
-    driver.findElement(PassWord).sendKeys("123456789");
-    driver.findElement(LoginButton).submit();
-
-    By SearchField= By.xpath("//input[@class='form-control input-lg']");
-    driver.findElement(SearchField).sendKeys(input);
-    
     }
+
+
+
+    @Test(priority = 2, dataProvider = "searchData")
+    public void Search(String searchTerm) {
+        driver.findElement(By.name("search")).clear();
+        driver.findElement(By.name("search")).sendKeys(searchTerm, Keys.ENTER);
+
+    }
+
+    @AfterClass
+    public void After(){
+        driver.quit();
+
+    }
+
+
 }
